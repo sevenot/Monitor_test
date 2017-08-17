c605566c0862a1d149845c403b04352abf2b95d9
sevenot <sevenot@126.com>
2017-08-17 23:42:18 +0800

    file monitor_main update most finished

--- monitor_main.sh
+++ monitor_main.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #主控脚本
+while true
+do
+
 echo -e "\033[1;31mWelcome to the main monitor shell,please read the content following! \033[0m"
 num_scripts=$(ls . | grep .sh | grep -v main | wc -l)
 echo -e "\033[1;31mNow we provide $num_scripts scripts: \033[0m"
@@ -13,18 +16,26 @@ for i in $(ls . | grep .sh | grep -v main)
 		num=$(($num+1))
 	done
 
-while true
-	do
-		echo -e "\033[1;31mWelcome to the main monitor shell,please read the content following! \033[0m"
-  		num_scripts=$(ls . | grep .sh | grep -v main | wc -l)
- 		echo -e "\033[1;31mNow we provide $num_scripts scripts: \033[0m"
 	
-		read -p "Please input the script you want to exec( 0 - $(($num_scripts-1)) ) : " cho
-		for((i=0;i<$num_scripts;i=i+1))
-			do
-				if [ $cho == $i ]
-					then
-						bash $(pwd)/${Arr[i]}
-				fi
-			done
-	done
+	read -p "Please input the script you want to exec( 0 - $(($num_scripts-1));q to quit) : " cho
+	if [ $cho == "q" ]
+		then
+			exit 0
+	fi
+
+
+	for((i=0;i<$num_scripts;i=i+1))
+		do
+			if [ $cho == $i ]
+				then
+					bash $(pwd)/${Arr[i]}
+			fi
+		done
+
+
+
+	echo
+	echo
+	echo
+	echo "###################################################################################"
+done
