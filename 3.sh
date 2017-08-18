f7827031235a5ff9b20a6d8e774bc00658ff97ef
sevenot <sevenot@126.com>
2017-08-18 16:51:36 +0800

    file docker_monitor update

--- 3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#output 3
-echo "3"
--- docker_monitor.sh
+++ docker_monitor.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-#docker状态监视脚本
-docker ps -a
+#docker状态监控脚本
+	echo -e "\033[1;31mDocker containers status:\033[0m"
+
+#total containers
+	total=$(( $(docker ps -a | wc -l) -1 ))
+	echo -e "\033[32mTotal containers : \033[34m$total\033[0m"
+
+#running containers
+	run=$(netstat -tunpla | grep docker | wc -l)
+	echo -e "\033[32mRunning containers : \033[34m$run\033[0m"
+
+#Exited containers
+	exi=$(docker ps -a | grep Exited | wc -l)
+	echo -e "\033[31mExited containers : $exi\033[0m"
+	docker ps -a | grep Exited
