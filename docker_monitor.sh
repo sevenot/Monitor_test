e0d2e1c177f210bd69672141e55c33d512ba572f
sevenot <sevenot@126.com>
2017-08-21 15:35:33 +0800

    file docker_monitor update

--- docker_monitor.sh
+++ docker_monitor.sh
@@ -5,9 +5,11 @@
 #total containers
 	total=$(( $(docker ps -a | wc -l) -1 ))
 	echo -e "\033[32mTotal containers : \033[34m$total\033[0m"
-
+#containers with port to localhost
+	por=$(netstat -tunpla | grep docker | wc -l)
+	echo -e "\033[32mPort to localhost containers : \033[34m$por\033[0m"
 #running containers
-	run=$(netstat -tunpla | grep docker | wc -l)
+	run=$(( $(docker ps | wc -l) -1))
 	echo -e "\033[32mRunning containers : \033[34m$run\033[0m"
 
 #Exited containers
